e9d1948bd438cb69c4800dfb13133dc19bb89edd
kaia <kairoskaia815@gmail.com>
2024-01-28 14:55:45 -0600

    Lightmap Multithreading works
    oh yeah

--- object_scenes/lightmapCover/lightmap.gd
+++ object_scenes/lightmapCover/lightmap.gd
@@ -4,37 +4,37 @@ extends Node2D
 var size = 64
 
 var thread: Thread
-var mutex : Mutex
 
 var readyToGo :bool= false
 
 var newImg = null
 var newMask = null
 
-var targetPos := Vector2.ZERO
-
 func _ready():
 	GlobalRef.lightmap = self
-	#set_process(false)
+	set_process(false)
 	readyToGo = true
 
 func _process(delta):
 	sprite.texture = newImg
 	sprite.material.set_shader_parameter("mask_texture",newMask)
-	position = targetPos
 	
 func pushUpdate(planet,newPos):
 	if !readyToGo: return
-	var relativePos = (global_position - planet.global_position) + Vector2(1024,1024)
 	if thread != null:
-		thread.wait_to_finish()
-	mutex = Mutex.new()
+		if thread.is_alive():
+			return
+	var relativePos = (newPos - planet.global_position) + Vector2(1024,1024)
 	thread = Thread.new()
 	var newX = int(relativePos.x)/8
 	var newY = int(relativePos.y)/8
 	#updateLight(int(relativePos.x)/8,int(relativePos.y)/8,planet)
 	thread.start(updateLight.bind(newX,newY,planet,newPos))
-
+	thread.wait_to_finish()
+	position = newPos
+	sprite.texture = newImg
+	sprite.material.set_shader_parameter("mask_texture",newMask)
+	
 func updateLight(x,y,planet,newPos):
 	
 	if !is_instance_valid(planet):
@@ -54,11 +54,6 @@ func updateLight(x,y,planet,newPos):
 			
 	newImg = ImageTexture.create_from_image(img)
 	newMask = ImageTexture.create_from_image(airImg)
-	
-	mutex.lock()
-	position = newPos
-	mutex.unlock()
-	
 	
 	return
 	
