0492ef5828286b62932167d3d22ee98ca3bbbe8c
kaia <kairoskaia815@gmail.com>
2024-02-01 21:18:03 -0600

    Update ItemBlock.gd

--- item_resources/item_classes/ItemBlock.gd
+++ item_resources/item_classes/ItemBlock.gd
@@ -7,10 +7,24 @@ class_name ItemBlock
 func onUse(tileX:int,tileY:int,planetDir:int,planet:Planet):
 	
 	if planet == null:
-		return
+		#Cancel if not on planet
+		return "failure"
 	
 	var planetData = planet.planetData
+	
+	if ![0,7].has(planetData[tileX][tileY][0]):
+		#Cancel is target tile isn't empty
+		return "failure"
+	
+	if ![0,7].has(planetData[tileX][tileY][1]):
+		#Succeed if wall tile exists
+		var edit = Vector3(tileX,tileY,0)
+		planet.editTiles({edit:blockID})
+		PlayerData.consumeSelected()
+		return "success"
+	
 	for i in range(4):
+		#Search neighboring tiles
 		var s = Vector2(1,0).rotated((PI/2)*i)
 		var tile = Vector2(tileX,tileY) + Vector2(int(s.x),int(s.y))
 		if tile.x < 0 or tile.x >= planetData.size() or tile.x < 0 or tile.x >= planetData.size():
@@ -20,4 +34,5 @@ func onUse(tileX:int,tileY:int,planetDir:int,planet:Planet):
 			planet.editTiles({edit:blockID})
 			PlayerData.consumeSelected()
 			return "success"
+	
 	return "failure"
